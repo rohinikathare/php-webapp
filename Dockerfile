56c8aacb2fef63a33477195b5491a9730cc01249
Ubuntu <ubuntu@ip-172-31-10-164.eu-north-1.compute.internal>
2026-01-06 11:55:42 +0000

    Initial PHP webapp with Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM devopsedu/webapp
+
+COPY index.php /var/www/html/index.php
+
+EXPOSE 80
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>PHP Web App - Jenkins CI/CD</h1>";
+echo "<p>Deployed via Jenkins Pipeline</p>";
+?>
